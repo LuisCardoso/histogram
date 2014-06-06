682a29056a2c20d401dad35b8586c14480ce2150
JavierJames <swifferayubu@gmail.com>
2014-06-06 21:42:53 +0200

    Created User Interface to fetch new data from user. Began also making a function that does this, so it can be called repeatedly. User gives Access-point by integer value, and bayesian recieves it as arraylist of string

--- src/histogram/Main.java
+++ src/histogram/Main.java
@@ -4,6 +4,8 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 import Localizer.Bayesian;
@@ -15,7 +17,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int User=0;  //0 = Javier pc, 1=Luis pc, 2=all phones,
+		//0 = Javier pc
+		//1=Luis pc
+		//2=all phones, note: un-comment instruction before running
+		
+		int User=0;  
 		
 		
 		String folder_base_path = null;
@@ -24,8 +30,9 @@ public class Main {
 			folder_base_path = "/home/swifferayubu/Dropbox/Doc/";
 		else if (User==1)
 			folder_base_path =	"/home/luis/Dropbox/School/Elective/Smart Phones Sensing/Doc/";
-		else if (User==2)
-			folder_base_path =	"/Downloads/";
+		else if (User==2){
+			//folder_base_path =	Environment.getExternalStorageDirectory().toString()+"/Download/";
+		}
 		
 		String folder_name = "cellsdata/";		//main folder
 	
@@ -90,19 +97,118 @@ public class Main {
 		Data_filterphase1 = rssi_filter.fetch_AP_and_RSSIavg(); //fetch access points from filter 1
 	    rssi_filter.filter_rssi_too_strong_Tree(Data_filterphase1);//filter data by rssi strength
 			
-	
-	    rssi_filter.display_normalAP();
-	    rssi_filter.save_filtered_AP();
+	   	rssi_filter.save_filtered_AP();
 	
 
 	    //step 6: Choose X amount of Access points 
-	    System.out.println("Choose some access points ");
-	    System.out.println("number access points chosen");
+    	ArrayList<Integer> chosen_ap = new ArrayList<Integer>(); //save list of chosen access points
+    	ArrayList<String> chosen_ap_names = new ArrayList<String>(); //save list of chosen access points
+    	
+    	rssi_filter.display_normalAP();
+    
+  
+	    Scanner keyboard = new Scanner(System.in);
+	    keyboard.useDelimiter("\\s*[,\n\r]\\s*");
+	   // Readable keyboard2 = new (System.in);
+	    
+	    int i=0;
+	   /*do{	 
+		   System.out.println("Choose some access points ");
+		    	
+	    	//chosen_ap.add(keyboard.nextLine());
+	    	chosen_ap.add(keyboard.next());
+	    	System.out.println(chosen_ap.get(i));
+	    	System.out.println(" i:"+i + "i++"+ ++i);
+	    	
+		  //  i++;
+	    }while(chosen_ap.get(i-1) != "3");
+	   //while(keyboard.next().length()>0);	  	    
+	  */  
+	  
+	   //read input as long enter is not pressed with with no accesspoint name
+	   //accesspoint captured  with pressing enter	    
+	   String data=null;
+	   int data_id;
+	  
+	   System.out.println("\n\n................User Interface Access-Points selection.................");
+	   System.out.println("Choose some access points by id number");
+	   System.out.println("Put id number then press Enter to put access-point");
+	   System.out.println("Press enter with no id, to finalize");
+	   System.out.println(".......................................................................\n\n\n");
+	   
+	   /* Tree map "id,String " */
+	   
+	   //number position of access-point in filtered access-point array list
+	   int ap_number;
+	   
+	   //read each line from user input, each line is ended with pressing enter
+	   //for each line read the number
+	   //stop reading, when no id is given and enter is pressed
+	   while(  (data=keyboard.nextLine()).length() > 0)
+	   {
+		   Scanner id_scanner = new Scanner(data); //read new line
+		
+		 //fetch number for that line. 
+		   //substract 1 since ap are displayed from 1 until X, instead from 0-X.
+		   ap_number = id_scanner.nextInt()-1;  
+		   
+		   chosen_ap.add(ap_number);
+	   }
+	   
+	    System.out.println("number access points chosen:" + chosen_ap.size());
+	    System.out.println("Display of chosen access-points");
+	    
+	    
+	    for(int j=0; j<chosen_ap.size(); j++)
+	    {
+	    	ap_number = chosen_ap.get(j);//get id name given by user
+	    	data= rssi_filter.get_list_of_normalAP().get(ap_number); //get ap name belonging to that ap id position 
+	    	chosen_ap_names.add(data);
+	    	
+	 //   	System.out.println("AP:"+ data);
+	    }
+	    
+	    
+	    for(int m=0; m<chosen_ap_names.size(); m++)
+	    {
+	    	System.out.println("AP array name: "+chosen_ap_names.get(m));
+	    }
+	    
 	    //display now the list of chosen access points 
 	 
-	    //read the accesspoints
-		
-		
+	    //keyboard.close();
+	    //read the access points
+		/* 
+		 * 
+		 * */
+	    
+	
+        // Testing nextLine();
+        /*System.out.print("\nWhat is your name? ");
+         name = keyboard.nextLine();
+ 
+        // Testing nextInt();
+        System.out.print("How many cats do you have? ");
+        int numberOfCats = keyboard.nextInt();
+ 
+        // Testing nextDouble();
+        System.out.print("How much money is in your wallet? $");
+        double moneyInWallet = keyboard.nextDouble();
+ 
+        System.out.println("\nHello " + name + "! You have " + numberOfCats
+                + (numberOfCats > 1 ? " cats" : " cat")
+                + " and $" + moneyInWallet + " in your wallet.\n");
+	    
+	    
+	    */
+	    
+	    
+	    
+	    keyboard.close();
+	    
+	    
+	    
+	    
 	    
 	    
 	    
@@ -146,7 +252,7 @@ public class Main {
 		
 	    
 		//begin classification
-		Bclassifier.bayesian_classify(false);
+		//Bclassifier.bayesian_classify(false);
 		
 		Bayesian Bclassifier_laplace = new Bayesian(ap1_pmf,ap2_pmf, ap3_pmf, ap4_pmf);
 		//Bclassifier_laplace.bayesian_classify(true); 
@@ -154,6 +260,23 @@ public class Main {
 		
 		
 		
+		
+		
+		
 	}
+	
 
+	public ArrayList<String> chooseNewAccessPoints()
+	{
+		//show list of all filtered access-points
+		
+		//show list of previously access-points
+		
+		//read new chosen access-points
+		
+		
+		//return chosen access-points
+		return null;
+	}
+	
 }
